4c29df0b1b519b8cb57cff6a8828fdc98f311ccf
rzuljevic <a.pejakovic@levi9.com>
2015-11-09 15:40:40 +0100

    Log parsing.

--- src/main/java/samza/streaming/client/StreamClient.java
+++ src/main/java/samza/streaming/client/StreamClient.java
@@ -1,20 +1,30 @@
 package samza.streaming.client;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.Stream;
 
 public class StreamClient {
     private final BlockingQueue<String> queue = new LinkedBlockingQueue<String>(100);
     private boolean active;
 
     public StreamClient() {
-    	try {
-    		// Parsiranje log fajla
-			queue.put("test");
-			active = true;
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+        try (Stream<String> stream = Files.lines(Paths.get("development.log"), Charset.defaultCharset())) {
+            stream.filter(e -> e != null || !e.isEmpty() || !e.startsWith("[")).forEach(e -> {
+                try {
+                    queue.put(e.substring(e.indexOf(']')));
+                } catch (InterruptedException ie) {
+                    ie.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        active = true;
     }
 
     public void stop() {
@@ -22,7 +32,7 @@ public class StreamClient {
     }
 
     public void stream(final LinkProcessor linkProcessor) {
-        while (! queue.isEmpty() && active){
+        while (!queue.isEmpty() && active){
         	try {
 				linkProcessor.processLink(queue.take());
 			} catch (InterruptedException e) {
