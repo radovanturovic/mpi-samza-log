c2e3d3a54af5a1de08879b9fc2717ff75f0ff960
Aleksandar Bircakovic <a.bircakovic@levi9.com>
2015-11-28 17:48:49 +0100

    Fixed issues with Java 8

--- src/main/java/samza/streaming/client/StreamClient.java
+++ src/main/java/samza/streaming/client/StreamClient.java
@@ -1,68 +1,64 @@
 package samza.streaming.client;
 
-import org.apache.log4j.Logger;
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.stream.Stream;
+
+import org.apache.log4j.Logger;
 
 public class StreamClient {
-    private static final Logger log = Logger.getLogger(StreamClient.class);
-    private final BlockingQueue<String> queue = new LinkedBlockingQueue<>(1000000);
-    private boolean active;
+	private static final Logger log = Logger.getLogger(StreamClient.class);
+	private final BlockingQueue<String> queue = new LinkedBlockingQueue<>(1000000);
+	private boolean active;
 
-    public StreamClient(String source) {
-        StringBuilder stringBuilder = new StringBuilder();
-        active = true;
-        try (Stream<String> stream = Files.lines(Paths.get(source), Charset.defaultCharset())) {
-            stream.forEach(e -> {
-                if (e.startsWith("\n") || e == null || e.isEmpty()) {
-                    try {
-                        queue.put(stringBuilder.toString());
-                    } catch (InterruptedException ie) {
-                        log.error("Error trying to send message to queue {}", ie);
-                    } finally {
-                        stringBuilder.setLength(0);
-                    }
-                } else {
-                    final int index = e.indexOf("]") + 1;
-                    stringBuilder.append(index > 0 && e.startsWith("[") ? e.substring(index) + "\n" : e + "\n");
-                }
-            });
-        } catch (IOException e) {
-            log.error("Some IOException: {}", e);
-        }
-        /*
-        //for testing purposes
+	public StreamClient(String source) {
+
+		StringBuilder stringBuilder = new StringBuilder();
 		active = true;
-    	try {
-			queue.put("test");
-		} catch (InterruptedException e) {
-			log.error("Some random error :D {}", e);
+		try (BufferedReader br = new BufferedReader(new FileReader(source))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				if (line.isEmpty()) {
+					try {
+						queue.put(stringBuilder.toString());
+					} catch (InterruptedException ie) {
+						log.error("Error trying to send message to queue {}", ie);
+					} finally {
+						stringBuilder.setLength(0);
+					}
+				} else {
+					final int index = line.indexOf("]") + 1;
+					stringBuilder
+							.append(index > 0 && line.startsWith("[") ? line.substring(index) + "\n" : line + "\n");
+				}
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		*/
-    }
+	}
 
-    public void stop() {
-    	active = false;
-    }
+	public void stop() {
+		active = false;
+	}
 
-    public void stream(final LinkProcessor linkProcessor) {
-        while (!queue.isEmpty() && active){
-        	try {
+	public void stream(final LinkProcessor linkProcessor) {
+		while (!queue.isEmpty() && active) {
+			try {
 				linkProcessor.processLink(queue.take());
 			} catch (InterruptedException e) {
 				log.error("Exception in stream: {}", e);
 			}
-        }
-    }
+		}
+	}
 
-    public interface LinkProcessor {
-        void processLink(String link);
-    }
+	public interface LinkProcessor {
+		void processLink(String link);
+	}
 
 }
--- src/main/java/samza/streaming/task/DataStreamTask.java
+++ src/main/java/samza/streaming/task/DataStreamTask.java
@@ -7,72 +7,84 @@ import org.apache.samza.system.SystemStream;
 import org.apache.samza.task.MessageCollector;
 import org.apache.samza.task.StreamTask;
 import org.apache.samza.task.TaskCoordinator;
+import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import samza.model.ProductionLog;
 
 import java.io.IOException;
 
 public class DataStreamTask implements StreamTask {
-    private static final Logger log = Logger.getLogger(DataStreamTask.class);
-    private static final SystemStream OUTPUT_STREAM = new SystemStream("kafka", "links-raw");
+	private static final Logger log = Logger.getLogger(DataStreamTask.class);
+	private static final SystemStream OUTPUT_STREAM = new SystemStream("kafka", "links-raw");
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+	private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    static {
-        objectMapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    }
+	static {
+		objectMapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+	}
 
-    @Override
-    public void process(IncomingMessageEnvelope envelope, MessageCollector collector, TaskCoordinator coordinator) {
-        final String logLines = (String) envelope.getMessage();
-        if (logLines.toLowerCase().contains("error"))
-            return;
+	@Override
+	public void process(IncomingMessageEnvelope envelope, MessageCollector collector, TaskCoordinator coordinator) {
+		final String logLines = (String) envelope.getMessage();
+		if (logLines.toLowerCase().contains("error"))
+			return;
 
-        final String[] lines = logLines.split("\n");
-        ProductionLog productionLog = new ProductionLog();
-        productionLog.setContent(lines.length + "");
+		final String[] lines = logLines.split("\n");
+		ProductionLog productionLog = new ProductionLog();
 
-        String trimmedLine;
+		String trimmedLine;
+		try {
+			for (String line : lines) {
+				trimmedLine = line.trim();
+				if (trimmedLine.startsWith("Processing")) {
+					processDate(trimmedLine, productionLog);
+				} else if (trimmedLine.startsWith("Parameters")) {
+					processJson(trimmedLine, productionLog);
+				} else if (trimmedLine.startsWith("Completed")) {
+					processLink(trimmedLine, productionLog);
+				}
+			}
+			collector.send(new OutgoingMessageEnvelope(OUTPUT_STREAM, objectMapper.writeValueAsString(productionLog)));
+		} catch (IOException e) {
+			log.error("Error while producing json string: {}", e);
+		}
+	}
 
-        for (String line : lines) {
-            trimmedLine = line.trim();
-            if (trimmedLine.startsWith("Processing")) {
-                processDate(trimmedLine, productionLog);
-            } else if (trimmedLine.startsWith("Parameters")) {
-                processJson(trimmedLine, productionLog);
-            } else if (trimmedLine.startsWith("Completed")) {
-                processLink(trimmedLine, productionLog);
-            }
-        }
+	private void processDate(String line, ProductionLog productionLog) {
+		try {
+			final String ip = line.substring(line.indexOf("("), line.indexOf(")"));
+			final String[] parts = ip.split(" ");
+			productionLog.putParameter("id", parts[1]);
+			productionLog.setDate(parts[2] + parts[3]);
+		} catch (Exception e) {
 
-        try {
-            collector.send(new OutgoingMessageEnvelope(OUTPUT_STREAM, objectMapper.writeValueAsString(productionLog)));
-        } catch (IOException e) {
-            log.error("Error while producing json string: {}", e);
-        }
-    }
+		}
+	}
 
-    private void processDate(String line, ProductionLog productionLog) {
-        final String ip = line.substring(line.indexOf("("), line.indexOf(")"));
-        final String[] parts = ip.split(" ");
-        productionLog.putParameter("id", parts[1]);
-        productionLog.setDate(parts[2] + parts[3]);
-    }
+	private void processLink(String line, ProductionLog productionLog) {
+		try {
+			final int index = line.indexOf("[");
+			final String response = line.substring(line.indexOf("|") + 2, index).trim();
+			final String link = line.substring(index + 1, line.length() - 1);
+			productionLog.putParameter("link", link);
+			productionLog.setResponse(response);
+		} catch (Exception e) {
 
-    private void processLink(String line, ProductionLog productionLog) {
-        final int index = line.indexOf("[");
-        final String response = line.substring(line.indexOf("|") + 2, index).trim();
-        final String link = line.substring(index + 1, line.length() - 1);
-        productionLog.putParameter("link", link);
-        productionLog.setResponse(response);
-    }
+		}
+	}
 
-    private void processJson(String line, ProductionLog productionLog) {
-        final String jsonString = line.substring(line.indexOf("{") + 1, line.indexOf("}"));
-        for (String keyValuePair : jsonString.split(",")) {
-            String[] splitted = keyValuePair.trim().split("=>");
-            productionLog.putParameter(splitted[0].trim().substring(1, splitted[0].length() - 1), splitted[1].substring(1, splitted[1].length() - 1));
-        }
-    }
+	private void processJson(String line, ProductionLog productionLog) {
+		try {
+			final String jsonString = line.substring(line.indexOf("{") + 1, line.indexOf("}"));
+			for (String keyValuePair : jsonString.split(",")) {
+				String[] splitted = keyValuePair.trim().split("=>");
+				productionLog.putParameter(splitted[0].trim().substring(1, splitted[0].length() - 1),
+						splitted[1].substring(1, splitted[1].length() - 1));
+			}
+		} catch (Exception e) {
+
+		}
+	}
 }
